bde0040b2906556bc7e45ff6f093609ce4884cea
José Júnior <136363382+josejunior2023@users.noreply.github.com>
2025-02-11 22:11:17 -0300

    Upd Firebase Config

---
+++ expo-example/firebase/config/firebaseConfig.ts
@@ -0,0 +1,19 @@
+import { FirebaseOptions } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyDxxFFCUUKlPkZqU-Q1c8H9a0BUMVwuLd4",
+
+  authDomain: "physical-activity-manager.firebaseapp.com",
+
+  projectId: "physical-activity-manager",
+
+  storageBucket: "physical-activity-manager.firebasestorage.app",
+
+  messagingSenderId: "796541474741",
+
+  appId: "1:796541474741:web:3333f042d281d1d3d5abfc",
+
+  measurementId: "G-CDZB09K1B6",
+};
+
+export default firebaseConfig;
